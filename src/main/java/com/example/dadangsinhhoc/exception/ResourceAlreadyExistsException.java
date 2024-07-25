7672ed5b18a1c838b4850ddc98b14b575a74dc2c
batruong1704 <batruong1704@gmail.com>
2024-07-25 16:10:16 +0700

    exception handling

---
+++ src/main/java/com/example/dadangsinhhoc/exception/ResourceAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.example.dadangsinhhoc.exception;
+
+// Xử lý ngoại lệ tồn tại bản ghi
+public class ResourceAlreadyExistsException extends Exception {
+    public ResourceAlreadyExistsException() {
+    }
+
+    public ResourceAlreadyExistsException(String msg) {
+        super(msg);
+    }
+}
