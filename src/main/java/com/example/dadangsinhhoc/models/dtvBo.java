ccab48ea5a8ef6f1c2d97ae83a089e478a2aac0e
batruong1704 <batruong1704@gmail.com>
2024-07-25 16:18:08 +0700

    patch for CRUD dtv bo

--- src/main/java/com/example/dadangsinhhoc/models/dtvBo.java
+++ src/main/java/com/example/dadangsinhhoc/models/dtvBo.java
@@ -1,6 +1,8 @@
 package com.example.dadangsinhhoc.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 import java.time.LocalDateTime;
@@ -9,6 +11,7 @@ import java.time.LocalDateTime;
 @Data
 public class dtvBo {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
