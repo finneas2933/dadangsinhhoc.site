b5321329bfcf7d1a0cd7a9fe3ea374fe5b2e28a7
batruong1704 <batruong1704@gmail.com>
2024-07-25 14:36:15 +0700

    swagger integration patch

--- src/main/java/com/example/dadangsinhhoc/config/SwaggerConfig.java
+++ src/main/java/com/example/dadangsinhhoc/config/SwaggerConfig.java
@@ -8,7 +8,6 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -18,11 +17,26 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.dadangsinhhoc"))
-                .paths(PathSelectors.regex("/.*"))
-                .build();
+        try {
+            Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.basePackage("com.example.dadangsinhhoc"))
+                    .paths(PathSelectors.regex("/.*"))
+                    .build().apiInfo(apiInfo());;
+            System.out.println("Swagger UI in: http://localhost:8080/swagger-ui/index.html#/");
+
+            return docket;
+        } catch (Exception e) {
+            System.err.println("Failt when init Swagger: " + e.getMessage());
+            throw e;
+        }
     }
 
-
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Geeks API")
+                .description("Demonstration of all write portal API")
+                .version("1.0")
+                .build();
+    }
 }
