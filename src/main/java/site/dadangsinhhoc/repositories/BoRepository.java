0e3dddace553952b10b2f3f058794a0ec3432438
batruong1704 <batruong1704@gmail.com>
2024-08-03 16:15:53 +0700

    1-commit

--- src/main/java/site/dadangsinhhoc/repositories/BoRepository.java
+++
@@ -1,9 +0,0 @@
-package site.dadangsinhhoc.repositories;
-
-import org.springframework.stereotype.Repository;
-import site.dadangsinhhoc.models.BoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface BoRepository extends JpaRepository<BoModel, Long> {
-}
